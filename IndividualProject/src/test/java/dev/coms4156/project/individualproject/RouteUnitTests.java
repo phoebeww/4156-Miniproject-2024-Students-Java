6ce9ac302e8dd59341271af00d9fc8041a6bc1c5
Phoebe Wang <kw3036@columbia.edu>
2024-09-11 15:26:19 -0400

    route test finished

--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/RouteUnitTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/RouteUnitTests.java
@@ -3,6 +3,7 @@ package dev.coms4156.project.individualproject;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -127,16 +128,6 @@ public class RouteUnitTests {
 
     String content3 = mvcResult3.getResponse().getContentAsString();
     assertEquals("Department Not Found", content3);
-
-//    when(myFileDatabase.getDepartmentMapping()).thenThrow(new RuntimeException("Test Exception"));
-//    MvcResult mvcResult4 = mockMvc.perform(get("/retrieveCourse")
-//        .param("deptCode", "COMS")
-//        .param("courseCode", String.valueOf(3157))
-//        .accept(MediaType.APPLICATION_JSON_VALUE))
-//        .andExpect(status().isInternalServerError()).andReturn();
-//
-//    String content4 = mvcResult4.getResponse().getContentAsString();
-//    assertEquals("An Error has occurred", content4);
   }
 
   @Test
@@ -180,7 +171,6 @@ public class RouteUnitTests {
 
     String content2 = mvcResult2.getResponse().getContentAsString();
     assertEquals("Department Not Found", content2);
-
   }
 
   @Test
@@ -202,7 +192,116 @@ public class RouteUnitTests {
 
     String content2 = mvcResult2.getResponse().getContentAsString();
     assertEquals("Department Not Found", content2);
+  }
 
+  @Test
+  public void courseLocationTest() throws Exception {
+    MvcResult mvcResult1 = mockMvc.perform(get("/findCourseLocation")
+        .param("deptCode", "COMS")
+        .param("courseCode", String.valueOf(3157))
+        .accept(MediaType.APPLICATION_JSON_VALUE))
+        .andExpect(status().isOk()).andReturn();
+
+    String content1 = mvcResult1.getResponse().getContentAsString();
+    String expected = myFileDatabase.getDepartmentMapping().get("COMS").getCourseSelection()
+        .get(Integer.toString(3157)).getCourseLocation() + " is where the course " + "is located.";
+    assertEquals(expected, content1);
+
+    MvcResult mvcResult2 = mockMvc.perform(get("/findCourseLocation")
+        .param("deptCode", "COMS")
+        .param("courseCode", String.valueOf(1234))
+        .accept(MediaType.APPLICATION_JSON_VALUE))
+        .andExpect(status().isNotFound()).andReturn();
+
+    String content2 = mvcResult2.getResponse().getContentAsString();
+    assertEquals("Course Not Found", content2);
+  }
+
+  @Test
+  public void findInstructorTest() throws Exception {
+    MvcResult mvcResult1 = mockMvc.perform(get("/findCourseInstructor")
+        .param("deptCode", "COMS")
+        .param("courseCode", String.valueOf(3157))
+        .accept(MediaType.APPLICATION_JSON_VALUE))
+        .andExpect(status().isOk()).andReturn();
+
+    String content1 = mvcResult1.getResponse().getContentAsString();
+    String expected = myFileDatabase.getDepartmentMapping().get("COMS").getCourseSelection()
+        .get(Integer.toString(3157)).getInstructorName()
+        + " is the instructor for" + " the course.";
+    assertEquals(expected, content1);
+
+    MvcResult mvcResult2 = mockMvc.perform(get("/findCourseInstructor")
+        .param("deptCode", "COMS")
+        .param("courseCode", String.valueOf(1234))
+        .accept(MediaType.APPLICATION_JSON_VALUE))
+        .andExpect(status().isNotFound()).andReturn();
+
+    String content2 = mvcResult2.getResponse().getContentAsString();
+    assertEquals("Course Not Found", content2);
+  }
+
+  @Test
+  public void findTimeSlotTest() throws Exception {
+    MvcResult mvcResult1 = mockMvc.perform(get("/findCourseTime")
+        .param("deptCode", "COMS")
+        .param("courseCode", String.valueOf(3157))
+        .accept(MediaType.APPLICATION_JSON_VALUE))
+        .andExpect(status().isOk()).andReturn();
+
+    String content1 = mvcResult1.getResponse().getContentAsString();
+    String expected = "The course meets at: " + myFileDatabase.getDepartmentMapping().get("COMS")
+        .getCourseSelection().get(Integer.toString(3157)).getCourseTimeSlot();
+    assertEquals(expected, content1);
+
+    MvcResult mvcResult2 = mockMvc.perform(get("/findCourseTime")
+        .param("deptCode", "COMS")
+        .param("courseCode", String.valueOf(1234))
+        .accept(MediaType.APPLICATION_JSON_VALUE))
+        .andExpect(status().isNotFound()).andReturn();
+
+    String content2 = mvcResult2.getResponse().getContentAsString();
+    assertEquals("Course Not Found", content2);
+  }
+
+  @Test
+  public void addMajorTest() throws Exception {
+    MvcResult mvcResult1 = mockMvc.perform(patch("/addMajorToDept")
+        .param("deptCode", "COMS")
+        .accept(MediaType.APPLICATION_JSON_VALUE))
+        .andExpect(status().isOk()).andReturn();
+
+    String content1 = mvcResult1.getResponse().getContentAsString();
+    String expected = "Attribute was updated successfully";
+    assertEquals(expected, content1);
+
+    MvcResult mvcResult2 = mockMvc.perform(patch("/addMajorToDept")
+        .param("deptCode", "CS")
+        .accept(MediaType.APPLICATION_JSON_VALUE))
+        .andExpect(status().isNotFound()).andReturn();
+
+    String content2 = mvcResult2.getResponse().getContentAsString();
+    assertEquals("Department Not Found", content2);
+  }
+
+  @Test
+  public void removeMajorTest() throws Exception {
+    MvcResult mvcResult1 = mockMvc.perform(patch("/removeMajorFromDept")
+        .param("deptCode", "COMS")
+        .accept(MediaType.APPLICATION_JSON_VALUE))
+        .andExpect(status().isOk()).andReturn();
+
+    String content1 = mvcResult1.getResponse().getContentAsString();
+    String expected = "Attribute was updated or is at minimum";
+    assertEquals(expected, content1);
+
+    MvcResult mvcResult2 = mockMvc.perform(patch("/removeMajorFromDept")
+        .param("deptCode", "CS")
+        .accept(MediaType.APPLICATION_JSON_VALUE))
+        .andExpect(status().isNotFound()).andReturn();
+
+    String content2 = mvcResult2.getResponse().getContentAsString();
+    assertEquals("Department Not Found", content2);
   }
 
 }
